49b4214e4a5760665aab53c1fe5f0a060ac36450
dragonone2 <96326628+dragonone2@users.noreply.github.com>
2022-11-21 20:44:48 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,91 +1,25 @@
-import React from 'react-native';
-import { View, Text, Image, ScrollView, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainQuestion1 from './Screens/MainQuestion1';
+import SelectStrategy1 from './Screens/SelectStrategy1';
+import Question1_1_1 from './Screens/Question1_1_1'
+import Question1_1_2 from './Screens/Question1_1_2'
 
-const App = () => {
+const Stack = createStackNavigator();
+export default function App() {
   return (
-    <View style={styles.grid}>
-        <Text style={styles.h1}>Quiz</Text>
-    <ScrollView>
-    
-      <View style={styles.vs}>
-        <Text style ={styles.question}>Todd orders pictures from a photographer. 
-            Each picture costs $7.50.
-             A one-time shipping fee of $3.25 is added to the cost of the order.
-            The total cost of Todd’s order before tax is $85.75. 
-            How many pictures did Todd order?</Text>
-        
-      </View>
-      <Text style={styles.vs2}>
-        <TouchableOpacity 
-            style={styles.answer}
-            onPress={()=>alert('정답입니다')}>정답1</TouchableOpacity>
-        <TouchableOpacity 
-            style={styles.answer}
-            onPress={()=>alert('오답입니다')}>정답2</TouchableOpacity>        
-        <TouchableOpacity 
-            style={styles.answer}
-            onPress={()=>alert('오답입니다')}>정답3</TouchableOpacity>
-        <TouchableOpacity 
-            style={styles.answer}
-            onPress={()=>alert('오답입니다')}>정답4</TouchableOpacity>
-      </Text>
-    </ScrollView>
-    </View>
-
-    
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "MainQuestion 1" component={MainQuestion1}/>
+        <Stack.Screen name = "SelectStrategy1" component={SelectStrategy1}/>
+        <Stack.Screen name = "Question1_1_1" component={Question1_1_1}/>
+        <Stack.Screen name = "Question1_1_2" component={Question1_1_2}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    //<MainQuestion1/>
+  );
 }
+//#수정사항
+//TextInput이 줄바꿈이 안됨
 
-const styles = StyleSheet.create({
-    grid:{
-        flex:1,
-    padding:5,
-    margin: 50,
-    width: '600px',
-    backgroundColor: "#fff",
-    border: '2px solid #eed0dc',
-    borderRadius:10,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    alignItems:'center',
-    },
-    h1:{
-        
-        alignItems:"center",
-        fontSize:40,
-        
-    },
-    vs:{
-        backgroundColor: '#fa8072',
-        border: '2px solid #eed0dc',
-        borderRadius:10,
-        alignItems:'center',
-    },
-    question:{
-        color:'#fff',
-        margin:10,
-        fontSize: 20,
-        
-    },
-    vs2:{
-        padding:'30px',
-        border: '2px solid #eed0dc',
-        borderRadius: 10,
-    },
-    answer:{
-        backgroundColor:'#eed0dc',
-        fontSize:15,
-        border: '1px solid #ffe3ed',
-        borderRadius: 5,
-        marginBottom:20,
-        marginRight:200,
-    }
-    
-})
 
-export default App;
